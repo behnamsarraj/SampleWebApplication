bac167bc8e2a7d3130957c2b184e05e3b8fec1f0
Behnam Sarraj <behnam.sarraj@gmail.com>
2021-12-10 16:56:34 +0330

    Add Personel Model

---
+++ SampleWebApplication/Model/Personel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleWebApplication.Model
+{
+    public class Personel
+    {
+        public int PersonelId { get; set; }
+        public string FirstName { get; set; }
+        public int Age { get; set; }
+    }
+}
